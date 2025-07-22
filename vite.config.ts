c6f5bcf56513511b7421e54184ba338c3b258abf
Carlos Sánchez <carlosasp-13@hotmail.com>
2025-07-22 04:04:03 -0500

    cambio en package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // 👈 SOLUCIÓN para rutas relativas en Netlify
+  base: './',
   plugins: [react()],
 });
